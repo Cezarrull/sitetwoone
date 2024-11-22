f7fdec7183a70052425487f5eb39c293d4ac53c7
Cezar rull <cezarrull12345@gmail.com>
2024-11-22 14:08:13 -0300

    :wrench: test two from the function CountDays

--- src/Pages/Pedido/Pedido.jsx
+++ src/Pages/Pedido/Pedido.jsx
@@ -17,7 +17,7 @@ export function Pedido() {
                                 E foi no dia 01 de novembro de 2024, que convidei a mulher dos meus sonhos, para ser o amor da minha vida!
                             </p>
                             <p>
-                                Estamos juntos a <span className='buttonDate' id='days' >{result}</span> dias!
+                                Estamos juntos a <span className='buttonDate' id='days'>{`${result}`}</span> dias!
                                 Que venha muitos outros.
                             </p>
                         </div>
