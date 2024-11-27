37b1298f6accb80d5b6ab0ab1d6675beb848ec32
Cezar rull <cezarrull12345@gmail.com>
2024-11-27 15:53:25 -0300

    :wrench: test Number

--- src/Pages/Pedido/Pedido.jsx
+++ src/Pages/Pedido/Pedido.jsx
@@ -17,7 +17,7 @@ export function Pedido() {
                                 E foi no dia 01 de novembro de 2024, que convidei a mulher dos meus sonhos, para ser o amor da minha vida!
                             </p>
                             <p>
-                                Estamos juntos a <span className='buttonDate' id='days'>{Number(result)}</span> dias!
+                                Estamos juntos a <span className='buttonDate' id='days'>{message}</span> dias!
                                 Que venha muitos outros.
                             </p>
                         </div>
@@ -56,6 +56,4 @@ export function Pedido() {
 
     const diffInDaysValue = diffInDays.toFixed() 
 
-    const message = Number(diffInDaysValue);
-
-    const result = message;
+    const message = Number.parseInt(diffInDaysValue);
