6d366d6a6c581fb24e2fac94d466a69b1488c334
Cezar rull <cezarrull12345@gmail.com>
2024-12-06 15:15:33 -0300

    :wrench::art: test new page and style

--- src/Pages/Notes/Notes.jsx
+++ src/Pages/Notes/Notes.jsx
@@ -2,16 +2,34 @@ import { Header } from "../../Components/Header/Header";
 import './Notes.css'
 import bgMobile from '../../Assets/bgImageMobile.jpg'
 import bgImage from '../../Assets/bgImage.jpg'
+import { Link } from "react-router-dom";
+import { ArrowLeft } from "@phosphor-icons/react";
+
+import PostItBack from '../../Assets/Notas/postitBack.png'
+import PostIt from '../../Assets/Notas/postit.png'
 
 export function Notes() {
     return(
         <>
         <Header />
+        <Link to="/" className="buttonBack"><ArrowLeft size={16}/>Voltar</Link>
         <img src={bgImage} alt="" className='bg'/>
         <img src={bgMobile} alt="" className='bgMobile'/>
+
         
             <div className="noteBody">
-
+                <div className="containerNote">
+                    <div className="contentNote">
+                        <img src={PostItBack} alt="" className="postBack"/>
+                            <p className="textNote">test test</p>
+                        <img src={PostIt} alt="" className="postUp"/>
+                    </div>
+                    <div className="contentNote">
+                        <img src={PostItBack} alt="" className="postBack"/>
+                            <p className="textNote">test test</p>
+                        <img src={PostIt} alt="" className="postUp"/>
+                    </div>
+                </div>
             </div>
         </>
     )
