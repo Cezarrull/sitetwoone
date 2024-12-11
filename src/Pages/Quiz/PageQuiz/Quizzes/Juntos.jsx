7e8b9b0bbecbab2e027443698237c912f8f00b58
Cezar rull <cezarrull12345@gmail.com>
2024-12-11 15:08:38 -0300

    :wrench::art: test new style response quiz

--- src/Pages/Quiz/PageQuiz/Quizzes/Juntos.jsx
+++ src/Pages/Quiz/PageQuiz/Quizzes/Juntos.jsx
@@ -1,14 +1,31 @@
 import { ButtonUp } from "../../../../Components/ButtonUp/ButtonUp";
 import { Header } from "../../../../Components/Header/Header";
 import './Quizzes.css'
+import { useState } from "react";
 
 import bgMobile from '../../../../Assets/bgImageMobile.jpg'
 import bgImage from '../../../../Assets/bgImage.jpg'
 
 import { Link } from "react-router-dom";
-import { ArrowLeft } from "@phosphor-icons/react";
+import { ArrowLeft, CaretUp } from "@phosphor-icons/react";
 
 export function Juntos() {
+    const [active, setActive] = useState(null)
+
+    const handleClick = (index) => {
+        if (active !== index) {
+            setActive(index);
+        } 
+    };
+
+    const btnClose = (index) => {
+        event.stopPropagation(); 
+
+        if (active === index) {
+            setActive(null);
+        }
+    };
+
     return(
         <>
             <Header />
@@ -22,21 +39,39 @@ export function Juntos() {
                     <div className="questionContent">
                         <h2 className="titleQuestion">BLA BLABLA BA BLA?</h2>
                         <div className="question">
-                            <p>test test test</p>
+                            <p><span>1)</span>testOne</p>
+                            <p><span>2)</span>testTwo</p>
+                            <p><span>3)</span>testThree</p>
                         </div>
-                        <div className="question">
-                            <p>test test</p>
-                        </div>
-                        <div className="question">
-                            <p>test</p>
-                        </div>
-                        <div className="buttonCheckQuestion">
-                            <div className="Response">
+                        <div className={`buttonCheckQuestion ${active === 0 ? 'buttonOpenResponse' : ''}`} onClick={() => handleClick(0)}>
+                            <p>RESPOSTA</p>
+                            <div className="response">
                                 <p className="textResponse">
                                     Porque test test test é test por causa do test,
                                     se test test test for test então dia test é test.
                                 </p>
                             </div>
+                            <div className="buttonCloseResponse" onClick={(event) => btnClose(0, event)}><CaretUp size={24} color="white" /></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="containerQuizzes">
+                    <div className="questionContent">
+                        <h2 className="titleQuestion">BLA BLABLA BA BLA?</h2>
+                        <div className="question">
+                            <p><span>1)</span>testOne</p>
+                            <p><span>2)</span>testTwo</p>
+                            <p><span>3)</span>testThree</p>
+                        </div>
+                        <div className={`buttonCheckQuestion ${active === 1 ? 'buttonOpenResponse' : ''}`} onClick={() => handleClick(1)}>
+                            <p>RESPOSTA</p>
+                            <div className="response">
+                                <p className="textResponse">
+                                    Porque test test test é test por causa do test,
+                                    se test test test for test então dia test é test.
+                                </p>
+                            </div>
+                            <div className="buttonCloseResponse" onClick={(event) => btnClose(1, event)}><CaretUp size={24} color="white" /></div>
                         </div>
                     </div>
                 </div>
