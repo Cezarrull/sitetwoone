ffe717cde813432f26a044fa3a772a55d7a8f0c8
Cezar rull <cezarrull12345@gmail.com>
2024-12-20 14:56:27 -0300

    :wrench: save PedidoFotos

--- src/Pages/Galeria/PedidoFotos/PedidoFotos.jsx
+++ src/Pages/Galeria/PedidoFotos/PedidoFotos.jsx
@@ -40,85 +40,85 @@ export function PedidoFotos () {
             <img src={bgImage} alt="" className='bg'/>
             <img src={bgMobile} alt="" className='bgMobile'/>
 
-                <div className="pedidoFotosBody">
-                    <div className='pedidoFotosContent'>
-                        <Link to="#">
-                            <figure className={`boxOpenedPedido ${active === 0 ? 'boxCheckPedido' : ''}`} onClick={() => handleClick(0)}>
-                                <img src={Image1} alt="" />
-                                <div className="buttonClosePedido" onClick={(event) => btnClose(0, event)}>
-                                    <X size={32}/>
-                                </div>
+            <div className="pedidoFotosBody">
+                <div className='pedidoFotosContent'>
+                    <Link to="#">
+                        <figure className={`boxOpenedPedido ${active === 0 ? 'boxCheckPedido' : ''}`} onClick={() => handleClick(0)}>
+                            <img src={Image1} alt="" />
+                            <div className="buttonClosePedido" onClick={(event) => btnClose(0, event)}>
+                                <X size={32}/>
+                            </div>
 
-                                <figcaption className="details">
-                                    <h2>01/09/2024</h2>
-                                </figcaption>
-                            </figure>
-                        </Link>
-                        <Link to="#">
-                            <figure className={`boxOpenedPedido ${active === 1 ? 'boxCheckPedido' : ''}`} onClick={() => handleClick(1)}>
-                                <img src={Image2} alt="" />
-                                <div className="buttonClosePedido" onClick={(event) => btnClose(1, event)}>
-                                    <X size={32}/>
-                                </div>
+                            <figcaption className="details">
+                                <h2>01/09/2024</h2>
+                            </figcaption>
+                        </figure>
+                    </Link>
+                    <Link to="#">
+                        <figure className={`boxOpenedPedido ${active === 1 ? 'boxCheckPedido' : ''}`} onClick={() => handleClick(1)}>
+                            <img src={Image2} alt="" />
+                            <div className="buttonClosePedido" onClick={(event) => btnClose(1, event)}>
+                                <X size={32}/>
+                            </div>
 
-                                <figcaption className="details">
-                                    <h2>01/09/2024</h2>
-                                </figcaption>
-                            </figure>
-                        </Link>
-                        <Link to="#">
-                            <figure className={`boxOpenedPedido ${active === 2 ? 'boxCheckPedido' : ''}`} onClick={() => handleClick(2)}>
-                                <img src={Image3} alt="" />
-                                <div className="buttonClosePedido" onClick={(event) => btnClose(2, event)}>
-                                    <X size={32}/>
-                                </div>
+                            <figcaption className="details">
+                                <h2>01/09/2024</h2>
+                            </figcaption>
+                        </figure>
+                    </Link>
+                    <Link to="#">
+                        <figure className={`boxOpenedPedido ${active === 2 ? 'boxCheckPedido' : ''}`} onClick={() => handleClick(2)}>
+                            <img src={Image3} alt="" />
+                            <div className="buttonClosePedido" onClick={(event) => btnClose(2, event)}>
+                                <X size={32}/>
+                            </div>
 
-                                <figcaption className="details">
-                                    <h2>01/09/2024</h2>
-                                </figcaption>
-                            </figure>
-                        </Link>
-                        <Link to="#">
-                            <figure className={`boxOpenedPedido ${active === 3 ? 'boxCheckPedido' : ''}`} onClick={() => handleClick(3)}>
-                                <img src={Image4} alt="" />
-                                <div className="buttonClosePedido" onClick={(event) => btnClose(3, event)}>
-                                    <X size={32}/>
-                                </div>
+                            <figcaption className="details">
+                                <h2>01/09/2024</h2>
+                            </figcaption>
+                        </figure>
+                    </Link>
+                    <Link to="#">
+                        <figure className={`boxOpenedPedido ${active === 3 ? 'boxCheckPedido' : ''}`} onClick={() => handleClick(3)}>
+                            <img src={Image4} alt="" />
+                            <div className="buttonClosePedido" onClick={(event) => btnClose(3, event)}>
+                                <X size={32}/>
+                            </div>
 
-                                <figcaption className="details">
-                                    <h2>01/09/2024</h2>
-                                </figcaption>
-                            </figure>
-                        </Link>
-                        <Link to="#">
-                            <figure className={`boxOpenedPedido ${active === 4 ? 'boxCheckPedido' : ''}`} onClick={() => handleClick(4)}>
-                                <img src={Image5} alt="" />
-                                <div className="buttonClosePedido" onClick={(event) => btnClose(4, event)}>
-                                    <X size={32}/>
-                                </div>
+                            <figcaption className="details">
+                                <h2>01/09/2024</h2>
+                            </figcaption>
+                        </figure>
+                    </Link>
+                    <Link to="#">
+                        <figure className={`boxOpenedPedido ${active === 4 ? 'boxCheckPedido' : ''}`} onClick={() => handleClick(4)}>
+                            <img src={Image5} alt="" />
+                            <div className="buttonClosePedido" onClick={(event) => btnClose(4, event)}>
+                                <X size={32}/>
+                            </div>
 
-                                <figcaption className="details">
-                                    <h2>01/09/2024</h2>
-                                </figcaption>
-                            </figure>
-                        </Link>
-                        <Link to="#">
-                            <figure className={`boxOpenedPedido ${active === 5 ? 'boxCheckPedido' : ''}`} onClick={() => handleClick(5)}>
-                                <img src={Image6} alt="" />
-                                <div className="buttonClosePedido" onClick={(event) => btnClose(5, event)}>
-                                    <X size={32}/>
-                                </div>
+                            <figcaption className="details">
+                                <h2>01/09/2024</h2>
+                            </figcaption>
+                        </figure>
+                    </Link>
+                    <Link to="#">
+                        <figure className={`boxOpenedPedido ${active === 5 ? 'boxCheckPedido' : ''}`} onClick={() => handleClick(5)}>
+                            <img src={Image6} alt="" />
+                            <div className="buttonClosePedido" onClick={(event) => btnClose(5, event)}>
+                                <X size={32}/>
+                            </div>
 
-                                <figcaption className="details">
-                                    <h2>01/09/2024</h2>
-                                </figcaption>
-                            </figure>
-                        </Link>
-                        <div className="PedidoLinkPage">
-                            <Link to="/Pedido" className='buttonPagePedido'>Vejas mais <ArrowSquareOut size={22} className='iconButtonPedido'/></Link>
-                        </div>
+                            <figcaption className="details">
+                                <h2>01/09/2024</h2>
+                            </figcaption>
+                        </figure>
+                    </Link>
+                    <div className="PedidoLinkPage">
+                        <Link to="/Pedido" className='buttonPagePedido'>Vejas mais <ArrowSquareOut size={22} className='iconButtonPedido'/></Link>
                     </div>
                 </div>
+            </div>
         </>
     )
 }
